c9e326aaaf7b1dc1d1d6fc55bac8470d3652edca
Mohammad Javad Bagheri Nejad <git@mjbn.ir>
2022-05-18 23:26:54 +0430

    finishing the startDl()

--- comicdl.py
+++ comicdl.py
@@ -5,6 +5,7 @@ from sys import argv, exit
 from PyQt5.QtWidgets import QApplication,QMainWindow,QWidget
 from PyQt5.QtWidgets import *
 from ui_comicdl import Ui_comicdl
+from wget import download
 
 
 class comicdl(QMainWindow, Ui_comicdl):
@@ -18,33 +19,35 @@ class comicdl(QMainWindow, Ui_comicdl):
         self.progressBar.setValue(0)
         # Window Title
         self.setWindowTitle('Comic DL')
-        # Window Properties
-        self._createStatusBar()
-
-    def _createStatusBar(self):
-        status = QStatusBar()
-        status.showMessage("I'm the Status Bar")
-        self.setStatusBar(status)
 
     def changeSites(self):
         pass
 
     def startDl(self):
         img_links = self.getImgLinks()
-        steps = img_links.index() + 2
+        pecentPerSteps = int(100/(len(img_links) + 2))
+        self.progressBar.setValue(pecentPerSteps)
+        i = 1
+        for link in img_links:
+            file = open('./'+str(i)+'.jpg', 'wb')
+            file.write(get(link).content)
+            file.close()
+            self.statusbar.showMessage(' ' + str(i*pecentPerSteps) +'% - ')
+            i+=1
+        self.statusbar.showMessage(' ' + str(i*pecentPerSteps) +'% - Done')
 
-    def progress(self):
-        self.progressBar.setValue(0)
+    def progress(self,i):
+        pass
 
     def getImgLinks(self):
         url = self.lineEdit.text()
         page = get(url)
         soup = BeautifulSoup(page.content, "html.parser")
         results = soup.find("div", class_="reading-content")
-        img_tag = results.find_all("img")
+        img_tags = results.find_all("img")
         img_links = []
-        for i in img_tag.index():
-            img_links.append(img_tag[i]["data-src"])
+        for img_tag in img_tags:
+            img_links.append(img_tag['data-src'])
         return img_links
 
     
